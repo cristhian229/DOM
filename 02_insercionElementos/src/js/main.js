90f4ab240c8f968b2386616ba686327cfd78a266
cristhian229 <cristhianvasquez99@gmail.com>
2024-04-30 13:42:00 -0500

    for loop

--- 02_insercionElementos/src/js/main.js
+++ 02_insercionElementos/src/js/main.js
@@ -16,3 +16,42 @@ header.classList.add("bg-dark","pb-5")
 
 
 header.appendChild(titulo)
+
+
+
+
+
+
+
+
+
+
+
+const main = document.querySelector("main")
+main.innerHTML =`
+  <article class="card" style="width: 18rem;">
+  <img src="https://imagenes.eltiempo.com/files/image_1200_600/uploads/2021/11/11/618da2f0f2822.jpeg"
+    class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Rigoberto</h5>
+    <p class="card-text">Raza: Pug <br>
+    Edad: 2 años</p>
+    <a href="#" class="btn btn-primary">Adoptar</a>
+  </div>
+  </article>
+`
+
+for (let i = 0; i < 50; i++) {
+  main.innerHTML +=`
+    <article class="card" style="width: 18rem;">
+    <img src="https://imagenes.eltiempo.com/files/image_1200_600/uploads/2021/11/11/618da2f0f2822.jpeg"
+      class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Rigoberto</h5>
+      <p class="card-text">Raza: Pug <br>
+      Edad: 2 años</p>
+      <a href="#" class="btn btn-primary">Adoptar</a>
+    </div>
+    </article>
+  `
+}
