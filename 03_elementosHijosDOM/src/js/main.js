2de21528780952b91dc451344b7d8cab2cdf2af0
Juankyyy <juancamiloherrera960@gmail.com>
2024-05-06 12:37:33 -0500

    elementos hijos del dom

---
+++ 03_elementosHijosDOM/src/js/main.js
@@ -0,0 +1,38 @@
+const body = document.querySelector("body")
+const main = document.querySelector("main")
+
+const boton = document.querySelector('#boton')
+
+
+function createBtnSucces(){
+  const button = document.createElement("button")
+  button.setAttribute("type","button")
+  button.classList.add("btn","btn-success")
+  button.textContent = "Hello"
+  return button
+}
+
+createBtnSucces()
+
+boton.addEventListener('click', () => {
+    // const response = createBtnSucces()
+    // main.appendChild(response)
+    // main.innerHTML += `<button type= "button" class = "btn btn-success"> hello</button>`
+    
+    // const btnForElimination = selectOneBtn()
+    // main.removeChild(btnForElimination)
+
+    // const newButton = createBtnSucces()
+    // const existentBtn = selectOneBtn()
+    // main.replaceChild(newButton,existentBtn)
+
+    const nuevoBoton = createBtnSucces()
+    const btnReference = selectOneBtn()
+    main.insertBefore(nuevoBoton,btnReference)
+})
+
+function selectOneBtn (){
+  const btnFound = document.querySelector(".btn-info")
+  return btnFound
+}
+selectOneBtn()
